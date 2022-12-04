54aff20991e8fdbcc6326b61d828dc433f9ab5b9
Theo-Ross <TheoRoss99@icloud.com>
2022-12-04 13:18:55 +0000

    initial setup

---
+++ ticket-tracker/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import Ticket from "./components/Ticket/Ticket.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="ticket__container">
+        
+       <Ticket className="ticket"/>
+       <Ticket className="ticket"/>
+       <Ticket className="ticket"/>
+       <Ticket className="ticket"/>
+       <Ticket className="ticket"/>
+       <Ticket className="ticket"/>
+       <Ticket className="ticket"/>
+       <Ticket className="ticket"/>
+       <Ticket className="ticket"/>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
