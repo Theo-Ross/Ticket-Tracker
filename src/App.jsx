efb12bd6fc509a29cef779f10e5febfeeb6855d9
Theo-Ross <TheoRoss99@icloud.com>
2022-12-06 16:10:01 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import './App.scss';
 import Ticket from "./components/Ticket/Ticket"
 import team from "./Data/names"
+import SearchBar from "./components/SearchBar/SearchBar"
+
 
 
 function App() {
  
+const [searchTerm, setSearchTerm] = useState("");
+const [filtered, setFiltered] = useState("");
+
+
+const handleInput = (event) => {
+  
+  const cleanInput = event.target.value.toLowerCase();
+
+  setSearchTerm(cleanInput);
+
+
+  const filtered =  team.filtered(users => {
+
+    const userLowercase = users.name.toLowerCase()
+    return userLowercase.includes(searchTerm) && users
+  })
+
+  setFiltered(filtered)
+}
+
+
+
   const ticketJSX = team.map ((data) => {
     return <Ticket name={data.name} role={data.role} />
   })
@@ -12,7 +36,7 @@ function App() {
   return (
     <div className="App">
       <h1>Ticket Tracker</h1>
-
+      <div> <SearchBar handleInput={handleInput}/> </div>
     <div className='app__tickets'>
       {ticketJSX}
     </div>
